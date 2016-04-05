6eb1db8492db65a6fd55e14b3bcede50d605e6fb
yuchiz <zhu.yuchi@gmail.com>
2016-04-05 17:48:37 -0400

    Add messaging feature

---
+++ com.flatironschool.safenyc/com.flatironschool.safenyc/RUFIMessageViewController.h
@@ -0,0 +1,13 @@
+//
+//  RUFIMessageViewController.h
+//  com.flatironschool.safenyc
+//
+//  Created by Yuchi Zhu on 4/5/16.
+//  Copyright © 2016 Irina Kalashnikova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RUFIMessageViewController : UIViewController
+
+@end
