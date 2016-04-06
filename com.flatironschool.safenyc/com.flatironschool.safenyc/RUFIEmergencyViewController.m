97fb08b1d3d54d5189412330b9ae259e163039e1
SunFloweri <irinakalashnikova@gmail.com>
2016-04-06 16:06:51 -0400

    Add messages to the emergency button

--- com.flatironschool.safenyc/com.flatironschool.safenyc/RUFIEmergencyViewController.m
+++ com.flatironschool.safenyc/com.flatironschool.safenyc/RUFIEmergencyViewController.m
@@ -10,8 +10,11 @@
 #import <ContactsUI/ContactsUI.h>
 #import <Contacts/Contacts.h>
 #import <DKCircleButton/DKCircleButton.h>
+#import <CoreLocation/CoreLocation.h>
+#import <MessageUI/MessageUI.h>
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface RUFIEmergencyViewController () <CNContactPickerDelegate>
+@interface RUFIEmergencyViewController () <CNContactPickerDelegate, MFMessageComposeViewControllerDelegate>
 
 @property (strong, nonatomic) DKCircleButton *emergencyButton;
 @property (strong, nonatomic) DKCircleButton *person1;
@@ -105,7 +108,27 @@
     
     if(button == self.emergencyButton){
     
-        NSLog(@"Emergency!");
+        if(![MFMessageComposeViewController canSendText]) {
+            NSLog(@"This device can't send texts!");
+            return;
+        }
+        
+        MFMessageComposeViewController *composeVC = [[MFMessageComposeViewController alloc] init];
+        
+        composeVC.messageComposeDelegate = self;
+        //add recipients
+        composeVC.recipients = @[@"9176506024"];
+        //BODY MESSAGE
+        
+        composeVC.body = @"Hey! I am concerned about the neighboorhood I am in. Please check in on me, this is my location";
+        
+        BOOL addedAttachment = [self addLocationAttachmentToComposeViewController:composeVC displayName:@"My Location" location:CLLocationCoordinate2DMake(40.705268, -74.013986)];
+        
+        if(!addedAttachment) {
+            NSLog(@"Seems there was an issue adding the attachment :(");
+        }
+        
+        [self showViewController:composeVC sender:nil];
         
     } else if (button == self.person1){
         
@@ -185,6 +208,42 @@
     }];
 }
 
+# pragma mark - Emergency Button
+-(BOOL)addLocationAttachmentToComposeViewController:(MFMessageComposeViewController *)composeVC displayName:(NSString *)displayName location:(CLLocationCoordinate2D)location
+{
+    NSURL *templateURL = [[NSBundle mainBundle] URLForResource:@"location_template" withExtension:@"loc.vcf"];
+    
+    NSError *error = nil;
+    NSMutableString *template = [[NSString stringWithContentsOfURL:templateURL encoding:NSUTF8StringEncoding error:&error] mutableCopy];
+    
+    if(!template) {
+        NSLog(@"Error loading vcard template file: %@", error);
+        return NO;
+    }
+    
+    
+    NSString *latString = [NSString stringWithFormat:@"%.6f", location.latitude];
+    NSString *longString = [NSString stringWithFormat:@"%.6f", location.longitude];
+    
+    [template replaceOccurrencesOfString:@"$displayname$" withString:displayName options:0 range:NSMakeRange(0, template.length)];
+    
+    [template replaceOccurrencesOfString:@"$lat$" withString:latString options:0 range:NSMakeRange(0, template.length)];
+    
+    [template replaceOccurrencesOfString:@"$long$" withString:longString options:0 range:NSMakeRange(0, template.length)];
+    
+    
+    NSData *vcardData = [template dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+    
+    
+    NSString *filename = [NSString stringWithFormat:@"%@.loc.vcf", displayName];
+    
+    return [composeVC addAttachmentData:vcardData typeIdentifier:(NSString *)kUTTypeVCard filename:filename];
+}
+
+-(void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult)result
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
 
